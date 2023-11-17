2cba3ea0df0bf62b3c6d5c0795235566dbfdbfa7
CMIT <CMIT@DESKTOP-SOP4B3I>
2023-11-17 17:01:02 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace NotifyChangeEvent
+{
+     public class Employee : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string _fullName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return "Mr " + _name;
+            }
+            set
+            {
+                _fullName = value;
+            }
+        }
+    }
+}
